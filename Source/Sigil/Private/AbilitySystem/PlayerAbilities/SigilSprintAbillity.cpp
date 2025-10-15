982b02aecc9e5542b64449c76ebfb68a342b9d2b
Floren <magrinyafloren@gmail.com>
2025-10-15 17:43:59 +0200

    Sprint GA

---
+++ Source/Sigil/Private/AbilitySystem/PlayerAbilities/SigilSprintAbillity.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/PlayerAbilities/SigilSprintAbillity.h"
+
+#include "Characters/SigilPlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+USigilSprintAbillity::USigilSprintAbillity()
+{
+	bActivateAbilityOnGranted = false;
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void USigilSprintAbillity::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	ASigilPlayerCharacter* PlayerCharacter = GetPlayerCharacterFromInfo();
+
+	if (IsValid(PlayerCharacter))
+	{
+		PlayerCharacter->SetMovementState(EMovementState::Jogging);
+		PlayerCharacter->GetCharacterMovement()->MaxWalkSpeed = 800.0f;
+	}
+
+	CommitAbility(Handle, ActorInfo, ActivationInfo);
+}
+
+void USigilSprintAbillity::InputReleased(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	ASigilPlayerCharacter* PlayerCharacter = GetPlayerCharacterFromInfo();
+
+	if (IsValid(PlayerCharacter))
+	{
+		PlayerCharacter->SetMovementState(EMovementState::Walking);
+		PlayerCharacter->GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	}
+
+	EndAbility(Handle,ActorInfo, ActivationInfo, false ,  false);
+}
---
+++ Source/Sigil/Public/AbilitySystem/PlayerAbilities/SigilSprintAbillity.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/SigilPlayerAbility.h"
+#include "SigilSprintAbillity.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIGIL_API USigilSprintAbillity : public USigilPlayerAbility
+{
+	GENERATED_BODY()
+
+	USigilSprintAbillity();
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+};
